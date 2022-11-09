ae9aa96fac0c054b7b7780b46be75d444fa5b5c6
ketijakrista <ketijakrista@gmail.com>
2022-11-09 21:03:32 +0200

    feat: add login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { PlannerComponent } from './pages/planner/planner.component';
 
 @NgModule({
-  declarations: [AppComponent, PlannerComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    PlannerComponent,
+    HeaderComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
